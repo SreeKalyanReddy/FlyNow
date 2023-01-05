011ce0e0bed3a01f01fbfa3a89a0f22e35c6fd25
sreek <sreek@DESKTOP-LHIT62F>
2023-01-05 12:56:17 +0530

    First Commit

---
+++ FlyNow/src/test/java/com/kalyan/flynow/FlyNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalyan.flynow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
